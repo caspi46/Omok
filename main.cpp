1b7d725cbc5c76c0af5177aaa2d1fd4895c6897e
caspi46 <68092667+caspi46@users.noreply.github.com>
2022-09-01 13:16:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,18 +10,7 @@ using namespace std;
 
 int main()
 {
-    int set;
-    cout << "OMOK table (5x5, 6x6, 7x7): ";
-    cin >> set;
-    while (set < 5 || set > 7)
-    {
-        cout << "OMOK Table must be between 5x5 and 7x7" << endl;
-        cout << "OMOK table (5x5, 6x6, 7x7): ";
-        cin >> set;
-    }
-    OmokTable game(set);
-    game.makeTable();
-
-
+    OmokTable game(-1);
+    game.gameStart();
 
 }
