752d13f09db26ee5798ab2bf1ae6d5a450cfaf9e
caspi46 <68092667+caspi46@users.noreply.github.com>
2022-09-01 13:15:54 -0700

    Update OmokTable.h

--- OmokTable.h
+++ OmokTable.h
@@ -12,21 +12,32 @@ using namespace std;
 
 class OmokTable {
 private:
-    bool** u1;
-    bool** u2;
-    int set;
+    int** omokTable;
+    int set = -1;
     char alp[8] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-    int num[7] = {1, 2, 3, 4, 5, 6, 7};
-    void checkBool (int i, int j);
-    bool checkPlace(string p, int u);
-    bool checkWin();
+    //int num[7] = {1, 2, 3, 4, 5, 6, 7};
+
+
 
 public:
     OmokTable();
     OmokTable(int set);
     void askSet();
+    void setSet();
+    bool checkCross1();
+    bool checkCross2();
+    bool checkColumn();
+    bool checkRow();
+    bool checkDrew();
+    bool checkWin();
+    int checkPlayer();
+    void checkBool(int i, int j);
+    bool identifyPlayer(int p1, int p2);
+    int checkPlace(char p, int n);
     void askPlace();
     void makeTable();
+    void gameStart();
+
     ~OmokTable();
 };
 
